45ec8eb8205761e22b289276d5839663ea98fa0c
rmarnon <rmarnon@yahoo.com.br>
2020-08-28 19:59:07 -0300

    Resistor Color

---
+++ teams/padawan-2020-maringa/csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+public static class ResistorColor
+{    
+    public static List<string> colors = new List<string> { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+
+    public static int ColorCode(string color) => colors.FindIndex(x => x == color);        
+    
+    public static string[] Colors() => colors.ToArray();
+}
