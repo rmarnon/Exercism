31fd814f197c3ef7a774cbdcd5f0ba7ea02fb185
rmarnon <rmarnon@yahoo.com.br>
2020-08-30 13:31:40 -0300

    HighScores V2

--- teams/padawan-2020-maringa/csharp/high-scores/HighScores.cs
+++ teams/padawan-2020-maringa/csharp/high-scores/HighScores.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 public class HighScores
 {
-    public List<int> Score { get; set; } = new List<int>();
+    private static List<int> Score { get; set; } = new List<int>();
 
     public HighScores(List<int> list)
     {
@@ -16,25 +16,5 @@ public class HighScores
 
     public int PersonalBest() => Score.Max();
 
-    public List<int> PersonalTopThree()
-    {
-        Score.Sort();
-        List<int> _3Max = new List<int>();
-
-        if (Score.Count < 4)
-        {
-            for (int i = 1; i <= Score.Count; i++)
-            {
-                _3Max.Add(Score[Score.Count - i]);
-            }
-        }
-        else
-        {
-            for (int i = 1; i < 4; i++)
-            {
-                _3Max.Add(Score[Score.Count - i]);
-            }
-        }
-        return _3Max;
-    }
+    public List<int> PersonalTopThree() => Score.OrderByDescending(x => x).Take(3).ToList();
 }
