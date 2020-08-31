88a48cc06ebf515437c1d17921d39760782af6b7
rmarnon <rmarnon@yahoo.com.br>
2020-08-31 15:52:13 -0300

    Sum of Multiples

---
+++ teams/padawan-2020-maringa/csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    //public static int Sum(IEnumerable<int> multiples, int max) => multiples.Where(x => x <= max).Select(x => x * x).Sum();
+
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        int value = 0;
+        List<int> lista = new List<int>(); ;
+
+        foreach (var item in multiples)
+        {
+            if (item == 0)
+                break;
+
+            int divide = 1;
+            while (item * divide < max)
+            {
+                value = item * divide;
+                if (!lista.Contains(value))
+                {
+                    lista.Add(value);
+                }
+                divide++;
+            }
+        }
+        return lista.Sum();
+    }
+}
