3a0f9d57ff83b20fb1387408b824fa9a18024781
rmarnon <rmarnon@yahoo.com.br>
2020-09-01 20:46:58 -0300

    Armstrong Numbers

---
+++ teams/padawan-2020-maringa/csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        string numbers = number.ToString();
+        double count = 0;
+
+        foreach (char item in numbers)
+        {
+            count += Math.Pow(double.Parse(item.ToString()), numbers.Length);
+        }
+        return count == number;
+    }
+}
