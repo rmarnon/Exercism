07277df8f347cbe29db7c92221635d4c9ba4a9c0
rmarnon <rmarnon@yahoo.com.br>
2020-09-01 22:44:56 -0300

    Grains

---
+++ teams/padawan-2020-maringa/csharp/grains/Grains.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if (n > 0 && n < 65)
+        {
+            return (ulong)Math.Pow(2, n - 1);
+        }
+        else
+            throw new ArgumentOutOfRangeException();
+    }
+
+    public static ulong Total()
+    {
+        ulong count = 0;
+        for (int i = 1; i <= 64; i++)
+        {
+            count += Square(i);
+        }
+        return count;
+    }
+}
