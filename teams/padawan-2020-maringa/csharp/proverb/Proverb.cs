c6d49d4751c439f2a6193e019f51793eb3dd7131
rmarnon <rmarnon@yahoo.com.br>
2020-09-04 02:37:55 -0300

    Proverb

---
+++ teams/padawan-2020-maringa/csharp/proverb/Proverb.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+    {
+        string mask = "For want of a + the - was lost.";
+        string lastSubject = "And all for the want of a -.";
+        string[] frase = new string[subjects.Length];
+        int count = subjects.Length;
+
+        for (int i = 0; i < subjects.Length; i++) 
+        {
+            while (count != 1)
+            {
+                frase[i] = mask.Replace("+", subjects[i]).Replace("-", subjects[i + 1]);
+                count--;
+                i++;
+            }
+
+            if (count == 1)
+            {
+                frase[i] = lastSubject.Replace("-", subjects[0]);
+            }  
+        }
+        return frase;
+    }
+}
