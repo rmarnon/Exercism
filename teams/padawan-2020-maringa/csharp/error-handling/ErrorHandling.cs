c09a5c7404df4112af0d6fb3ee6249b315a3fb53
rmarnon <rmarnon@yahoo.com.br>
2020-09-11 02:37:55 -0300

    Error Handling

---
+++ teams/padawan-2020-maringa/csharp/error-handling/ErrorHandling.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException()
+    {
+        throw new Exception();
+    }
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        try
+        {
+            int x = int.Parse(input);
+            return x;
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result)
+    {
+        try
+        {
+            result = int.Parse(input);
+            return true;
+        }
+        catch (Exception)
+        {
+            result = 0;
+            return false;
+        }
+    }
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        try
+        {
+            disposableObject.Dispose();
+            throw new Exception();
+        }
+        catch (Exception)
+        {
+            throw new Exception();
+        }
+    }
+}
