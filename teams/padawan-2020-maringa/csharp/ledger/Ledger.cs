53821f2f74621651a9c92023804ad7ffd150d5dd
rmarnon <rmarnon@yahoo.com.br>
2020-09-18 09:41:05 -0300

    Ledger

---
+++ teams/padawan-2020-maringa/csharp/ledger/Ledger.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+public class LedgerEntry
+{
+    public LedgerEntry(DateTime date, string desc, decimal chg)
+    {
+        Date = date;
+        Desc = desc;
+        Chg = chg;
+    }
+
+    public DateTime Date { get; }
+    public string Desc { get; }
+    public decimal Chg { get; }
+}
+
+public static class Ledger
+{
+    public static LedgerEntry CreateEntry(string date, string desc, int chng) => new LedgerEntry(DateTime.Parse(date, CultureInfo.InvariantCulture), desc, chng / 100.0m);
+
+    private static CultureInfo CreateCulture(string cur, string loc)
+    {
+        if ((cur != "USD" && cur != "EUR") || (loc != "nl-NL" && loc != "en-US"))        
+            throw new ArgumentException("Invalid currency");       
+
+        var culture = new CultureInfo(loc);
+        culture.NumberFormat.CurrencySymbol = cur == "USD" ? "$" : "€"; 
+        culture.NumberFormat.CurrencyNegativePattern = loc != "en-US" ? 12 : 0; 
+        culture.DateTimeFormat.ShortDatePattern = loc != "en-US" ? "dd/MM/yyyy" : "MM/dd/yyyy"; 
+        return culture;
+    }
+
+    private static string PrintHead(string loc) => (loc == "en-US")
+            ? "Date       | Description               | Change       " : loc == "nl-NL"
+            ? "Datum      | Omschrijving              | Verandering  " : throw new ArgumentException("Invalid locale");
+
+    private static string Description(string desc) => desc.Length > 25 ? $"{desc.Substring(0, 22)}..." : desc;
+
+    private static string Change(IFormatProvider culture, decimal cgh) => cgh < 0.0m ? cgh.ToString("C", culture) : cgh.ToString("C", culture) + " ";
+
+    private static string PrintEntry(IFormatProvider culture, LedgerEntry entry) =>
+        $"{entry.Date.ToString("d", culture)} | {Description(entry.Desc),-25} | {Change(culture, entry.Chg),13}";
+
+    private static IEnumerable<LedgerEntry> sort(LedgerEntry[] entries)
+    {
+        var result = new List<LedgerEntry>();
+        result.AddRange(entries.Where(e => e.Chg < 0).OrderBy(x => $"{x.Date}@{x.Desc}@{x.Chg}"));
+        result.AddRange(entries.Where(e => e.Chg >= 0).OrderBy(x => $"{x.Date}@{x.Desc}@{x.Chg}"));
+
+        return result;
+    }
+
+    public static string Format(string currency, string locale, LedgerEntry[] entries)
+    {
+        string formatted = PrintHead(locale);
+        var culture = CreateCulture(currency, locale);
+
+        if (entries.Length > 0)
+        {
+            for (var i = 0; i < sort(entries).Count(); i++)
+            {
+                formatted += $"\n{PrintEntry(culture, sort(entries).Skip(i).First())}";
+            }
+        }
+
+        return formatted;
+    }
+}
---
+++ teams/padawan-2020-maringa/csharp/markdown/Markdown.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public static class Markdown
+{
+    private static string Wrap(string text, string tag) => $"<{tag}>{text}</{tag}>";
+
+    private static string Parse(string markdown, string delimiter, string tag)
+    {
+        return Regex.Replace(markdown, $"{delimiter}(.+){delimiter}", $"<{tag}>$1</{tag}>");
+    }
+
+    private static string Parse__(string markdown) => Parse(markdown, "__", "strong");
+
+    private static string Parse_(string markdown) => Parse(markdown, "_", "em");
+
+    private static string ParseText(string markdown, bool list)
+    {        
+        var parsedText = Parse_(Parse__((markdown)));
+
+        return list ? parsedText : Wrap(parsedText, "p");        
+    }
+
+    private static string ParseHeader(string markdown, bool list, out bool inListAfter)
+    {
+        var count = 0;
+
+        foreach (var item in markdown)
+        {
+            if (item == '#') 
+            {
+                count += 1;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        if (count == 0)
+        {
+            inListAfter = list;
+            return null;
+        }
+
+        var headerTag = "h" + count;
+        var headerHtml = Wrap(markdown.Substring(count + 1), headerTag);
+
+        if (list)
+        {
+            inListAfter = false;
+            return "</ul>" + headerHtml;
+        }
+        else
+        {
+            inListAfter = false;
+            return headerHtml;
+        }
+    }
+
+    private static string ParseLineItem(string markdown, bool list, out bool inListAfter)
+    {
+        if (markdown.StartsWith("*"))
+        {
+            var innerHtml = Wrap(ParseText(markdown.Substring(2), true), "li");
+
+            if (list)
+            {
+                inListAfter = true;
+                return innerHtml;
+            }
+            else
+            {
+                inListAfter = true;
+                return "<ul>" + innerHtml;
+            }
+        }
+
+        inListAfter = list;
+        return null;
+    }
+
+    private static string ParseParagraph(string markdown, bool list, out bool inListAfter)
+    {
+        if (!list)
+        {
+            inListAfter = false;
+            return ParseText(markdown, list);
+        }
+        else
+        {
+            inListAfter = false;
+            return "</ul>" + ParseText(markdown, false);
+        }
+    }
+
+    private static string ParseLine(string markdown, bool list, out bool inListAfter)
+    {
+        var result = ParseHeader(markdown, list, out inListAfter);
+
+        if (result == null)
+        {
+            result = ParseLineItem(markdown, list, out inListAfter);
+
+        }
+
+        if (result == null)
+        {
+            result = ParseParagraph(markdown, list, out inListAfter);
+        }
+
+        return result == null ? throw new ArgumentException("Invalid markdown") : result;
+    }
+
+    public static string Parse(string markdown)
+    {
+        var lines = markdown.Split('\n');
+        var result = "";
+        var list = false;
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            var lineResult = ParseLine(lines[i], list, out list);
+            result += lineResult;
+        }
+        return list ? result + "</ul>" : result;        
+    }
+}
